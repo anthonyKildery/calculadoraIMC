a6b19823ecbe789540bc22987f72dfff11b89cdc
anthonyKdl <anthonytecinf@gmail.com>
2021-10-09 22:27:20 -0300

    primeiro commit

--- js/main.js
+++ js/main.js
@@ -1,30 +1,55 @@
 
 const calcular = document.getElementById('calcular');
 
-function imc () {
-    const nome = document.getElementById('nome').value;
-    const altura = document.getElementById('altura').value;
+function imc() {
+    const masculino = document.getElementById('masculino');
+    const feminino = document.getElementById('feminino');
+    const idade = document.getElementById('idade').value;
+    const estatura = document.getElementById('estatura').value;
     const peso = document.getElementById('peso').value;
     const resultado = document.getElementById('resultado');
 
-    if (nome !== '' && altura !== '' && peso !== '') {
-        const valorIMC = (peso / (altura ** 2)).toFixed(2);
-
+    if (idade !== '' && estatura !== '' && peso !== '') {
+        const imc = (peso / (estatura * estatura)).toFixed(2);
+        let sexo = '';
         let classificacao = '';
-        if (valorIMC < 18.25) {
-            classificacao = 'Abaixo do pesso';
+        
+        if (masculino.checked == true) {
+            sexo = 'Masculino';
+
+            if (imc < 18.5) {
+                classificacao = 'abaixo do peso.';
+            }
+            else if (imc > 18.49 && imc < 25) {  
+                classificacao = 'peso normal.';
+            }
+            else if (imc > 25 && imc < 29.99) {    
+                classificacao = 'sobrepeso.';
+            }
+            else if (imc > 30) {
+                classificacao = 'obesidade.';
+            }
         }
-        else if (valorIMC < 25) {
-            classificacao = 'Pesso adequado';
-        }
-        else if (valorIMC < 30) {
-            classificacao = 'Levimente acima do peso';
-        }
-        else if (valorIMC < 35) {
-            classificacao = 'Obesidade Grau I';
+        else if (feminino.checked == true) {
+            sexo = 'Feminino';
+
+            if (imc < 18.5) {
+                classificacao = 'abaixo do peso.';
+            }
+            else if (imc > 18.49 && imc < 25) {  
+                classificacao = 'peso normal.';
+            }
+            else if (imc > 25 && imc < 29.99) {    
+                classificacao = 'sobrepeso.';
+            }
+            else if (imc > 30) {
+                classificacao = 'obesidade.';
+            }
         }
 
-        resultado.textContent = ('Seu IMC: ' +valorIMC);
+        let text = ('Seu IMC é de ' +imc+ '. Isso indica que você está classificado com ' +classificacao); 
+        document.getElementById('resultado').innerHTML = text;
+
     }else {
         resultado.textContent = 'Preencha todos os campos';
     }
